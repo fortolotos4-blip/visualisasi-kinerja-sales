9e8e55fdbff882695e3d80a1d9a78e50738d35c6
Rohani Game Dev <fortolotos4@gmail.com>
2026-01-21 12:06:14 +0700

    haha

--- app/Sales.php
+++ app/Sales.php
@@ -3,33 +3,40 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Wilayah; 
+use App\User;
+use App\SalesOrder;
+use App\TargetSales;
 
 class Sales extends Model
 {
-    //
     protected $table = 'sales';
-    protected $fillable = ['kode_sales','user_id', 'nama_sales', 'wilayah_id', 'target_penjualan'];
 
-public function wilayah()
-{
-    return $this->belongsTo(Wilayah::class, 'wilayah_id');
-}
-// App\Sales.php
-public function user()
-{
-    return $this->belongsTo(User::class, 'user_id');
-}
+    protected $fillable = [
+        'kode_sales',
+        'user_id',
+        'nama_sales',
+        'wilayah_id',
+        'target_penjualan'
+    ];
 
+    public function wilayah()
+    {
+        return $this->belongsTo(Wilayah::class, 'wilayah_id');
+    }
 
-// app/Sales.php
-public function salesOrder()
-{
-    return $this->hasMany(SalesOrder::class);
-}
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
-public function targetSales()
-{
-    return $this->hasMany(TargetSales::class);
-}
+    public function salesOrder()
+    {
+        return $this->hasMany(SalesOrder::class);
+    }
 
+    public function targetSales()
+    {
+        return $this->hasMany(TargetSales::class);
+    }
 }
