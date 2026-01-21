6cefab95cbaec3ff519bc4bf833160bf1a359ca5
Rohani Game Dev <fortolotos4@gmail.com>
2026-01-21 08:40:47 +0700

    walawee

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-composer install --no-dev --optimize-autoloader --no-scripts
-composer dump-autoload
+# Pastikan folder storage ada
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/framework/cache
+mkdir -p storage/logs
+
+# Permission agar Laravel bisa nulis
+chmod -R 775 storage bootstrap/cache
 
 php artisan config:clear
 php artisan cache:clear
-php artisan optimize
 
 php artisan serve --host=0.0.0.0 --port=10000
